3196ea374cd62c53465ac66914b572f87ed1ac27
Anthony Fairchild <fairchild.anthony@gmail.com>
2013-02-10 19:59:59 -0800

    Added a TODO to EXPT.

--- lunula.lisp
+++ lunula.lisp
@@ -883,6 +883,7 @@
   (< num 0))
 
 (defun expt (base power)
+  ;; TODO: power can only be an integer with the code below
   (let ((num 1)
         (op (if (< power 0)  #'/ #'*)))
     (dotimes (i (abs power))
