81afdb4a683c6c7ee7d4274510527a3736d20c57
Anthony Fairchild <anthonyf@blarg.net>
2013-02-21 22:24:28 -0800

    Reordered some code in mini CLOS implementation.

--- src/mini-clos.lisp
+++ src/mini-clos.lisp
@@ -11,6 +11,19 @@
 ;;; than :print-function functions, if you like (they will be
 ;;; inherited properly, and they don't have the silly DEPTH argument).
 
+(defun supertype (type)
+  "Find the most specific supertype of this type."
+  (cond ((eq type t) nil)
+        ((get type :supertype))
+        (t 'atom)))
+
+(defun call-method-for (name type var args)
+  "Find the method for this type, following :supertype links if needed."
+  (let ((m (get name type)))
+    (cond (m (apply m var args))
+          ((eq type nil) (error "Can't find method ~A for ~A." name var))
+          (t (call-method-for name (supertype type) var args)))))
+
 (defun ensure-generic-function (name)
   "Define NAME to be a generic function."
   (unless (eq (symbol-function name) (get name :generic))
@@ -28,18 +41,6 @@
   `(setf (get ',name :generic) (ensure-generic-function ',name)
          (get ',name ',class) #'(lambda (,var . ,other-args) . ,body)))
 
-(defun supertype (type)
-  "Find the most specific supertype of this type."
-  (cond ((eq type t) nil)
-        ((get type :supertype))
-        (t 'atom)))
-
-(defun call-method-for (name type var args)
-  "Find the method for this type, following :supertype links if needed."
-  (let ((m (get name type)))
-    (cond (m (apply m var args))
-          ((eq type nil) (error "Can't find method ~A for ~A." name var))
-          (t (call-method-for name (supertype type) var args)))))
 
 ;; Construct a small part of the built-in type hierarchy
 (mapc
