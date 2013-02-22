52e58d740da32cdd10af6a0021d1906306f4270d
Anthony Fairchild <anthonyf@blarg.net>
2013-02-22 03:15:54 +0000

    Updated DESTRUCTURING-BIND implementation.  Now using the one from CLTL2.

--- src/destructuring-bind.lisp
+++ src/destructuring-bind.lisp
@@ -1,6 +1,22 @@
 (in-package :lunula)
 
-(defmacro destructuring-bind (lambda-list expression &body body)
-  `(apply (lambda ,lambda-list
-            ,@body)
-          ,expression))
+;; stolen from CLTL2
+(defmacro destructuring-bind (lambda-list list &body body)
+  "Bind the variables in lambda-list to the result list and execute body."
+  ;; This implementation does not do the defmacro extensions,
+  ;; Except that it does handle a trailing dot: (x y . z)
+  (cond ((null lambda-list)
+         `(progn ,@body))
+        ((not (symbolp list))
+         (let ((var (gensym)))
+           `(let ((,var ,list))
+              (destructuring-bind ,lambda-list ,var ,@body))))
+        ((symbolp lambda-list)
+         `(let ((,lambda-list ,list)) ,@body))
+        ((atom lambda-list)
+         (error "Can't bind ~A to a value." lambda-list))
+        ((member (first lambda-list) '(&rest &optional &key &aux))
+         `(apply #'(lambda ,lambda-list ,@body) ,list))
+        (t `(destructuring-bind ,(first lambda-list) (first ,list)
+              (destructuring-bind ,(rest lambda-list) (rest ,list)
+                ,@body)))))
