aca1b00fa053d676a0254c845cf620c2b48740b8
Anthony Fairchild <anthonyf@blarg.net>
2013-02-28 05:45:13 +0000

    Some minor work on the compiler

--- src/compiler.lisp
+++ src/compiler.lisp
@@ -1,1 +1,10 @@
 (in-package :lunula)
+
+(defun compile-stream (input-stream output-stream)
+  (let ((*package* *package*))
+    ))
+
+(defun compile-string (str)
+  (with-input-from-string (in str)
+    (cl:with-output-to-string (out)
+      (compile-stream in out))))
