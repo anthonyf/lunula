e4f68ae6f43fdde90e3235fa88cc5b3a5d967485
Anthony Fairchild <anthonyf@blarg.net>
2013-03-03 12:06:37 -0800

    implemented find-unique-emitted-name in lexical environment

--- src/lexical-environment.lisp
+++ src/lexical-environment.lisp
@@ -4,8 +4,11 @@
   symbol
   emitted-name)
 
+(defvar *unique-var-name-counter* 0)
+
 (defun find-unique-emitted-name (env var)
-  )
+  (declare (ignore env var))
+  (concatenate 'string "l" (integer-to-string *unique-var-name-counter*)))
 
 (defmacro env-extend (env var)
   (let ((var-name (gensym)))
